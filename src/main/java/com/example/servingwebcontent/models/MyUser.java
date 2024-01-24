5c7b177b54a85f5da9bb7c5d28df015c009de1a2
pavel-burdakov <p.o.burdakov@yandex.ru>
2024-01-24 14:44:31 +0300

    добавляем секьюрити

--- src/main/java/com/example/servingwebcontent/models/MyUser.java
+++ src/main/java/com/example/servingwebcontent/models/MyUser.java
@@ -13,4 +13,28 @@ public class MyUser {
     private String name;
     private String password;
     private String roles;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
 }
